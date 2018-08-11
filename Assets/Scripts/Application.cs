464b555eb85257b5878d5400bf9705b458659df7
OlegKoposov <o.koposov@tarkov.com>
2018-08-11 19:06:20 +0300

    Application

---
+++ Assets/Scripts/Application.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+    private static Application _instance;
+    public static Application Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<Application>();
+                if (_instance == null)
+                {
+                    var go = new GameObject("___Application");
+                    _instance = go.AddComponent<Application>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private InputManager _input;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        if (_instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Units/Selectable.cs
+++ Assets/Scripts/Units/Selectable.cs
@@ -7,6 +7,6 @@ public class Selectable : MonoBehaviour
 
     public void Select()
     {
-
+        
     }
 }
