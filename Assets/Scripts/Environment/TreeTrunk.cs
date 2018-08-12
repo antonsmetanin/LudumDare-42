ed8fd709b0431731dc620fe351e02535553fbb3f
OlegKoposov <o.koposov@tarkov.com>
2018-08-12 22:27:45 +0300

    Selectable trunk

--- Assets/Scripts/Environment/TreeTrunk.cs
+++ Assets/Scripts/Environment/TreeTrunk.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TreeTrunk : MonoBehaviour
 {
-    public GameObject[] CollectObjects;
+    [Serializable]
+    public class CollectPoint
+    {
+        public Vector3 Point;
+        public float Count;
+    }
+
+    public CollectPoint[] CollectPoints;
     public bool IsDead;
 
     private void OnTriggerEnter(Collider other)
@@ -15,4 +23,30 @@ public class TreeTrunk : MonoBehaviour
             IsDead = true;
         }
     }
+
+    public CollectPoint GetCollectPoint()
+    {
+        return CollectPoints.FirstOrDefault(_ => _.Count > 0);
+    }
+
+    public Vector3 GetCollectPointPosition(CollectPoint cp)
+    {
+        return transform.position + cp.Point;
+    }
+
+    public float Collect(CollectPoint cp, float count)
+    {
+        var value = Mathf.Min(count, cp.Count);
+        cp.Count -= value;
+        return value;
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.magenta;
+    //    foreach (var cp in CollectPoints)
+    //    {
+    //        Gizmos.DrawWireSphere(transform.position + cp.Point, 0.1f);
+    //    }
+    //}
 }
