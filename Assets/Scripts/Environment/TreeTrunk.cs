141fb82ad1281392603fffff07359fe11618d057
OlegKoposov <o.koposov@tarkov.com>
2018-08-12 20:15:34 +0300

    rename tree trunk

---
+++ Assets/Scripts/Environment/TreeTrunk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTrunk : MonoBehaviour
+{
+
+    public GameObject[] CollectObjects;
+    public bool IsDead;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            IsDead = true;
+        }
+    }
+}
