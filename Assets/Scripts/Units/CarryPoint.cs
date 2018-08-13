15774226a3d26c2ef3b1d2722f552887d70850e4
OlegKoposov <o.koposov@tarkov.com>
2018-08-13 19:34:54 +0300

    trunk part

---
+++ Assets/Scripts/Units/CarryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryPoint : MonoBehaviour
+{
+
+
+
+    public void AcceptTrunk()
+    {
+
+    }
+}
