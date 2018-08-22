aaaf042beb3161ecbf6d45d855e876a017a788aa
Anton Smetanin <Anton.Smetanin@gmail.com>
2018-08-22 22:22:31 +0300

    Some UI improvements

--- Assets/Scripts/View/DraggedDataFileView.cs
+++ Assets/Scripts/View/DraggedDataFileView.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using JetBrains.Annotations;
 using Model;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace View
 {
@@ -10,18 +12,21 @@ namespace View
 	{
 		public Robot Robot;
 
-		public void Show(Game game, Robot robot, DataFileView.DataFileType type, Func<int> getBytes)
+        [SerializeField] private Color _leakColor;
+        [SerializeField] private Color _dataColor;
+
+        [SerializeField] private TextMeshProUGUI _label;
+        [SerializeField] private Image _image;
+
+        public void Show(Game game, Robot robot, DataFileView.DataFileType type, Func<int> getBytes)
 		{
 			Robot = robot;
 			((RectTransform)transform).sizeDelta = new Vector2((getBytes() * game.Template.MemoryIndicationScale), ((RectTransform)transform).sizeDelta.y);
-			GetComponentInChildren<TMPro.TextMeshProUGUI>().text = type == DataFileView.DataFileType.Leak ? "Leak" : "Produce";
-		}
-
-		public void Dispose() => Destroy(gameObject);
-
-		public void Drag([NotNull] PointerEventData eventData)
-		{
-            transform.position = eventData.position;
+			_label.text = type == DataFileView.DataFileType.Leak ? "leak" : "data";
+            _image.color = type == DataFileView.DataFileType.Leak ? _leakColor : _dataColor;
         }
-	}
+
+        public void Drag([NotNull] PointerEventData eventData) => transform.position = eventData.position;
+        public void Dispose() => Destroy(gameObject);
+    }
 }
